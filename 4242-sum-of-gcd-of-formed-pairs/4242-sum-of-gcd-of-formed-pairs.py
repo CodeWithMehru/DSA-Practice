6ea62be27392e9f3adf73b42af4fdb5f4310e7ed
MEHRAAN AMIN <mehru.codes@gmail.com>
2026-07-16 20:23:17 +0530

    4242-SUM-OF-GCD-OF-FORMED-PAIRS Stats: Time: 681 ms (93.75%), Space: 23.3 MB (25%) - LeetHub

--- 4242-sum-of-gcd-of-formed-pairs/4242-sum-of-gcd-of-formed-pairs.py
+++ 4242-sum-of-gcd-of-formed-pairs/4242-sum-of-gcd-of-formed-pairs.py
@@ -9,20 +9,20 @@ class Solution:
         prefix_gcd = [0] * n
         curr_max = nums[0]
         
-        # Step 1 & 2: Running max aur prefixGcd nikalna
+        
         for i in range(n):
             if nums[i] > curr_max:
                 curr_max = nums[i]
             prefix_gcd[i] = get_gcd(nums[i], curr_max)
             
-        # Step 3: Sort karna
+        
         prefix_gcd.sort()
         
         ans = 0
         left = 0
         right = n - 1
         
-        # Step 4: Smallest aur largest ka pair banake GCD sum karna
+        
         while left < right:
             ans += get_gcd(prefix_gcd[left], prefix_gcd[right])
             left += 1
